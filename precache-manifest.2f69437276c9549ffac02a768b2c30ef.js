0439c2b4bc5c3ec7b8741f9e4ccfd9780b828848
Benjamin Millagou myagoo <millagou.benjamin@gmail.com>
2020-10-09 00:14:59 +0200

    Updates

---
+++ precache-manifest.2f69437276c9549ffac02a768b2c30ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4336384b82cfc30a6ff6ec990d844e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f58d9151196d9493b14f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "98fdabe8a42776d371fa",
+    "url": "/static/js/2.c543338b.chunk.js"
+  },
+  {
+    "revision": "05b857f66ed133b0b0c06eef1a416fca",
+    "url": "/static/js/2.c543338b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58d9151196d9493b14f",
+    "url": "/static/js/main.4060adaf.chunk.js"
+  },
+  {
+    "revision": "a601f03b803600fe2644",
+    "url": "/static/js/runtime-main.4d72f3f7.js"
+  }
+]);
